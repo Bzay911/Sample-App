f0c801e1bfe0c1554efb198223802d3bde8899e0
bijay <12510@ait.nsw.edu.au>
2024-07-10 15:34:21 +1000

    working signup and added home route

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,58 @@
 import {View, Text, StyleSheet, StatusBar, TextInput} from 'react-native'
 import { Link } from 'expo-router'
 import { AuthForm } from '@/components/AuthForm'
+import { AuthContext } from '@/contexts/AuthContext'
+import { useContext } from 'react'
+import {createUserWithEmailAndPassword, Auth} from '@firebase/auth'
+import {router, useRouter} from 'expo-router'
+import { useEffect } from 'react'
 
-export default function Index() {
+export default function Signup(props:any) {
+    const auth = useContext( AuthContext )
+    const router = useRouter()
+
+    const createAccount = (email:string, password:string) =>{
+        createUserWithEmailAndPassword ( auth, email, password )
+        .then((userCredentials) => {
+            console.log(userCredentials.user)
+        })
+        .catch((error) => {
+            console.log(error.code, error.message)
+            router.replace('/home')
+
+        })
+     }
+
+     useEffect(()=> {
+        if(auth.currentUser){
+            router.replace('/home')
+        }
+
+     })
+
     return(
         <View>
-            <AuthForm title="Sign up an account" />
-            <Link href="/login">
-            <Text>Go to Sign in</Text>
-            </Link>
+             <View>
+            <AuthForm title="Sign up for an account" actionText="Sign up" action={createAccount}/>
+            <View style={ styles.container }>
+                <Text>Already have an account?</Text>
+                <Link href='/login'>
+                    <Text style={ styles.link } >Go to Sign in</Text>
+                </Link>
+            </View>
+        </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "center",
+        marginVertical: 15,
+    },
+    link: {
+        color: "#b8111e",
+        marginLeft: 5,
+    }
+})
