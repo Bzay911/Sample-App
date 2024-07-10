fce0c1972e6a2c4f0ec44b89fab3d5ebbcf5ed3c
bijay <12510@ait.nsw.edu.au>
2024-07-10 15:47:09 +1000

    adding auth router and sign out

--- app/home.tsx
+++ app/home.tsx
@@ -1,9 +1,32 @@
-import{View, Text, StyleSheet} from 'react-native'
+import{View, Text, StyleSheet, Pressable} from 'react-native'
+import { AuthContext } from '@/contexts/AuthContext'
+import { useContext, useEffect } from 'react'
+import {signOut} from '@firebase/auth'
+import { router, useRouter } from 'expo-router'
 
 export default function Home( props:any ){
+const auth = useContext(AuthContext)
+
+useEffect(()=>{
+    if(auth){
+        console.log(auth.currentUser)
+    }
+})
+const SignOutUser = () => { 
+    signOut(auth) 
+    .then(() => {
+        router.replace('/')
+    })
+    .catch((error) => {
+        console.log(error.code, error.message)
+    })
+}
     return(
         <View>
             <Text>Home</Text>
+            <Pressable onPress={() => SignOutUser()}>
+                <Text>Sign out</Text>
+            </Pressable>
         </View>
     )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,8 @@ import { Link } from 'expo-router'
 import { AuthForm } from '@/components/AuthForm'
 import { AuthContext } from '@/contexts/AuthContext'
 import { useContext } from 'react'
-import {createUserWithEmailAndPassword, Auth} from '@firebase/auth'
-import {router, useRouter} from 'expo-router'
-import { useEffect } from 'react'
+import {createUserWithEmailAndPassword, onAuthStateChanged} from '@firebase/auth'
+import {useRouter} from 'expo-router'
 
 export default function Signup(props:any) {
     const auth = useContext( AuthContext )
@@ -23,12 +22,16 @@ export default function Signup(props:any) {
         })
      }
 
-     useEffect(()=> {
-        if(auth.currentUser){
-            router.replace('/home')
-        }
 
-     })
+    onAuthStateChanged(auth, (user)=>{
+        if(user){
+            // user is authenticated
+            // redirect to home
+            router.replace('/home')
+        }else{
+            // user is no authenticated
+        }
+    })
 
     return(
         <View>
